39471cf52bff37563688c2f4219bd873bbe3df10
Aryashi Tripathi <143412573+t-aryashi@users.noreply.github.com>
2024-07-03 11:45:01 +0530

    Update settings.py

--- restaurant_service/restaurant_service/settings.py
+++ restaurant_service/restaurant_service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1geo&$mfwd@5^!azpmcquipbchg3(-2og11ys^v$^2_w4_g$6h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['t-aryashi.github.io', 'www.t-aryashi.github.io']
 
 
 # Application definition
